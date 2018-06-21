25ae50dfe0e40f089f292dd2c7c2a003eaffd9c0
Employee <employee@Justs-Mac-mini.local>
2018-06-21 17:16:29 +0300

    stage-3

--- jquery.exchange.js
+++ jquery.exchange.js
@@ -1,7 +1,29 @@
 (function($) {
-    let toValue = $('#to-value');
-    $.getJSON('http://free.currencyconverterapi.com/api/v5/convert?q=UAH_USD&compact=y', function(data) {
-        let dataStr = JSON.stringify(data);
-        toValue.html(dataStr);
+    const apiUrl = 'http://free.currencyconverterapi.com/api/v5/convert?q={from}_{to}&compact=y';
+    const $convertBtn = $('#convert');
+    const $fromCurrency = $('#from-currency');
+    const $toCurrency = $('#to-currency');
+    const $toValue = $('#to-value');
+
+    function getValue($elem) {
+        return $elem.val();
+    }
+
+    function getCurrentURL(apiUrl, from, to) {
+        return apiUrl.replace(/{from}/, from).replace(/{to}/, to);
+    }
+
+    function setCurrentJSON(url, $elem) {
+        $.getJSON(url, function(data) {
+            $elem.html(JSON.stringify(data));
+        });
+    }
+
+    $convertBtn.on('click', function(e) {
+        e.preventDefault();
+        let fromCurrencyVal = getValue($fromCurrency);
+        let toCurrencyVal = getValue($toCurrency);
+
+        setCurrentJSON(getCurrentURL(apiUrl, fromCurrencyVal, toCurrencyVal), $toValue);
     });
 })(jQuery);
