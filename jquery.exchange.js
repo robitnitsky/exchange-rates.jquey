bad860d54c6ebe4a42386ceb659ee891919c8cbf
Employee <employee@Justs-Mac-mini.local>
2018-06-21 17:33:15 +0300

    stage-4

--- jquery.exchange.js
+++ jquery.exchange.js
@@ -1,29 +1,31 @@
 (function($) {
-    const apiUrl = 'http://free.currencyconverterapi.com/api/v5/convert?q={from}_{to}&compact=y';
-    const $convertBtn = $('#convert');
-    const $fromCurrency = $('#from-currency');
-    const $toCurrency = $('#to-currency');
-    const $toValue = $('#to-value');
+    $.fn.exchange = function() {
+        const apiUrl = 'http://free.currencyconverterapi.com/api/v5/convert?q={from}_{to}&compact=y';
+        const $convertBtn = $('#convert');
+        const $fromCurrency = $('#from-currency');
+        const $toCurrency = $('#to-currency');
+        const $toValue = this;
 
-    function getValue($elem) {
-        return $elem.val();
-    }
+        function getValue($elem) {
+            return $elem.val();
+        }
 
-    function getCurrentURL(apiUrl, from, to) {
-        return apiUrl.replace(/{from}/, from).replace(/{to}/, to);
-    }
+        function getCurrentURL(apiUrl, from, to) {
+            return apiUrl.replace(/{from}/, from).replace(/{to}/, to);
+        }
 
-    function setCurrentJSON(url, $elem) {
-        $.getJSON(url, function(data) {
-            $elem.html(JSON.stringify(data));
+        function setCurrentJSON(url, $elem) {
+            $.getJSON(url, function(data) {
+                $elem.html(JSON.stringify(data));
+            });
+        }
+
+        $convertBtn.on('click', function(e) {
+            e.preventDefault();
+            let fromCurrencyVal = getValue($fromCurrency);
+            let toCurrencyVal = getValue($toCurrency);
+
+            setCurrentJSON(getCurrentURL(apiUrl, fromCurrencyVal, toCurrencyVal), $toValue);
         });
-    }
-
-    $convertBtn.on('click', function(e) {
-        e.preventDefault();
-        let fromCurrencyVal = getValue($fromCurrency);
-        let toCurrencyVal = getValue($toCurrency);
-
-        setCurrentJSON(getCurrentURL(apiUrl, fromCurrencyVal, toCurrencyVal), $toValue);
-    });
+    };
 })(jQuery);
