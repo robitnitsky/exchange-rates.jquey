2fc72cbef04d91607d8a8e54dbea7150e1521848
Employee <employee@Justs-Mac-mini.local>
2018-06-22 10:58:46 +0300

    stage-5

--- jquery.exchange.js
+++ jquery.exchange.js
@@ -2,8 +2,10 @@
     $.fn.exchange = function() {
         const apiUrl = 'http://free.currencyconverterapi.com/api/v5/convert?q={from}_{to}&compact=y';
         const $convertBtn = $('#convert');
+        const $versaBtn = $('#vice-versa-currency');
         const $fromCurrency = $('#from-currency');
         const $toCurrency = $('#to-currency');
+        const $fromValue = $('#from-value');
         const $toValue = this;
 
         function getValue($elem) {
@@ -14,18 +16,42 @@
             return apiUrl.replace(/{from}/, from).replace(/{to}/, to);
         }
 
-        function setCurrentJSON(url, $elem) {
+        function calculate(input, coeficient) {
+            return input * coeficient;
+        }
+
+        function reverseSelects(select1, select2) {
+            let select1Val = getValue(select1);
+            select1.val(getValue(select2));
+            select2.val(select1Val);
+        }
+
+        function setResult(apiUrl, fromCurrencyVal, toCurrencyVal, $elem) {
+            let url = getCurrentURL(apiUrl, fromCurrencyVal, toCurrencyVal);
             $.getJSON(url, function(data) {
-                $elem.html(JSON.stringify(data));
+                let currentKey = fromCurrencyVal.toUpperCase() + '_' + toCurrencyVal.toUpperCase();
+                let coeficient = data[currentKey].val;
+                $elem.html(calculate(getValue($fromValue), coeficient));
             });
         }
 
         $convertBtn.on('click', function(e) {
             e.preventDefault();
-            let fromCurrencyVal = getValue($fromCurrency);
-            let toCurrencyVal = getValue($toCurrency);
+            setResult(apiUrl, getValue($fromCurrency), getValue($toCurrency), $toValue);
+        });
 
-            setCurrentJSON(getCurrentURL(apiUrl, fromCurrencyVal, toCurrencyVal), $toValue);
+        $versaBtn.on('click', function(e) {
+            e.preventDefault();
+            reverseSelects($fromCurrency, $toCurrency);
+            setResult(apiUrl, getValue($fromCurrency), getValue($toCurrency), $toValue);
+        });
+
+        $fromCurrency.change(function() {
+            setResult(apiUrl, getValue($fromCurrency), getValue($toCurrency), $toValue);
+        });
+
+        $toCurrency.change(function() {
+            setResult(apiUrl, getValue($fromCurrency), getValue($toCurrency), $toValue);
         });
     };
 })(jQuery);
