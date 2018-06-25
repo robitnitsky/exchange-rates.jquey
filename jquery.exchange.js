c2d318ec8ef78b888cc3cd509b68abadb9cf42cd
Employee <employee@Justs-Mac-mini.local>
2018-06-25 13:54:56 +0300

    stage-8

--- jquery.exchange.js
+++ jquery.exchange.js
@@ -69,7 +69,110 @@
 
         methods.init();
 
-        return this;
+        $.widget('exchange.rateHistory', {
+            options: {
+                from: 'UAH',
+                to: 'USD',
+                days: 8,
+
+                change: null,
+            },
+
+            _create() {
+                this.element.css({
+                    paddingTop: 20,
+                    position: 'relative',
+                });
+                this._refresh();
+            },
+
+            _refresh() {
+                const dates = this._getDates();
+                const exchangeCode = `${this.options.from}_${this.options.to}`;
+                const url = `http://free.currencyconverterapi.com/api/v5/convert?q=${exchangeCode}&compact=y&date=${dates[0]}&endDate=${dates[dates.length - 1]}`;
+                console.log(url);
+
+                $.getJSON(url).then((response) => {
+                    const values = Object.entries(response[exchangeCode].val);
+
+                    this.element.html('');
+
+                    values.forEach((value) => {
+                        const maxCurrency = Math.max.apply(Math.max, values.map((value) => value[1]));
+                        const minCurrency = Math.min.apply(Math.min, values.map((value) => value[1]));
+                        const height = ((value[1] - minCurrency) / (maxCurrency - minCurrency)) * 300 - 20;
+
+                        $('<div>', {'class': 'chart-item'}).css({
+                            width: `calc(${100 / this.options.days}% - 10px)`,
+                            background: 'blue',
+                            height: `${height}`,
+                            borderTop: '1px solid #1e5799',
+                            marginLeft: 5,
+                            marginRight: 5,
+                            display: 'inline-block',
+                            position: 'relative',
+                            background: 'linear-gradient(to bottom, #1e5799 0%,#ffffff 100%)',
+                        }).appendTo(this.element).append(
+                            $('<div>').html(value[0]).css({
+                                position: 'absolute',
+                                bottom: -20,
+                                left: 0,
+                                right: 0,
+                                textAlign: 'center',
+                            }),
+                            $('<div>').html(value[1]).css({
+                                position: 'absolute',
+                                left: 0,
+                                right: 0,
+                                textAlign: 'center',
+                                top: () => {
+                                    if (height <= 19 || height.toString() === 'NaN') {
+                                        return 'auto';
+                                    } else {
+                                        return 0;
+                                    }
+                                },
+                                bottom: () => {
+                                    if (height <= 19 || height.toString() === 'NaN') {
+                                        return 100 + '%';
+                                    } else {
+                                        return 'auto';
+                                    }
+                                },
+                                color: () => {
+                                    if (height <= 19 || height.toString() === 'NaN') {
+                                        return 'black';
+                                    } else {
+                                        return 'white';
+                                    }
+                                },
+                            }),
+                        );
+                    });
+                });
+            },
+            _getDates: function() {
+                const dates = [];
+                for (let i = 0; i < this.options.days; i++) {
+                    const today = new Date();
+                    const date = new Date(today);
+
+                    date.setDate(today.getDate() - i);
+                    dates.unshift(date.toJSON().slice(0, 10));
+                }
+
+                return dates;
+            },
+
+            _setOptions: function(arguments) {
+                this._superApply(arguments);
+                this._refresh();
+            },
+
+            _destroy() {
+                this.element.html('');
+            },
+        });
     };
 
     $.fn.exchange.defaults = {
